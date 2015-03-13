67944235e60895714af7ab01e5f75e18c04162f5
rafavermo <rafavermo@gmail.com>
2015-03-13 15:02:55 +0100

    Resolución Ejercicio 3
    Resolución Ejercicio 3 del archivo while.c

--- ejercicios/3/while.c
+++ ejercicios/3/while.c
@@ -4,10 +4,19 @@
  */
 
 /* Bibliotecas a incluir */
+#include <stdio.h>
 
 void main()
 {
 	/* Declaración de variables */
+	int len = 0;
+	char cadena[] = "cadena\0";
 
 	/* Código usando while*/
+	while (cadena[len] != '\0') 
+	{
+		printf("El carácter es: %c", cadena[len]);
+		len++;		
+	}
+	printf("El número de caracteres es: %d", len);
 }
