fa98a15ca84aeb278def3145e157471ea5361009
rafavermo <rafavermo@gmail.com>
2015-03-13 15:17:13 +0100

    Resolución Ejercicio 9
    Resolución Ejercicio 9 del archivo utils.h

---
+++ ejercicios/9/utils.h
@@ -0,0 +1,10 @@
+/* Declarar en este fichero la función que queremos exportar a otros ficheros */
+int cuenta_numero_letras(char, char);
+int compara_cadena(char, char);
+int tipo_de_letra (char);
+int primera_letra(char, char);
+int cambia_letras(char, char, char);
+int suma_numeros (int, int);
+int resta_numeros (int, int);
+int multiplican_numeros (int, int);
+
