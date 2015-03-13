8da05f41dd8547bf0fcfa84b22cf141015fc40c0
rafavermo <rafavermo@gmail.com>
2015-03-13 15:11:21 +0100

    Resolución Ejercicio 7
    Resolución Ejercicio 7 del archivo main.c

--- ejercicios/7/main.c
+++ ejercicios/7/main.c
@@ -11,11 +11,26 @@
  */
 
 /* Zona para incluir las bibliotecas o las cabeceras */
+#include <stdio.h>
+#include "utils.h"
 
 /* Zona para declarar la funcion estática */
+static int multiplicar_numero_secreto(int numero) 
+{
+  int numero_secreto = 33;
+
+  if (es_par(numero))
+		return numero * numero_secreto;
+  else
+		return -1;
+}
 
 /* Programa Principal */
-void main ()
+void main()
 {
-
+	int numero = 25;
+	int multiplicacion = multiplicar_numero_secreto(numero);
+	
+	printf("La multiplicación del número '%d' con el numero secreto es: %d", numero, multiplicacion);
+		
 }
