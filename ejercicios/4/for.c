07c4d965a86ea90c2dd6ecdd60e86464973f7160
rafavermo <rafavermo@gmail.com>
2015-03-13 15:04:07 +0100

    Resolución Ejercicio 4
    Resolución Ejercicio 4 del archivo for.c

--- ejercicios/4/for.c
+++ ejercicios/4/for.c
@@ -4,10 +4,20 @@
 
 
 /* Bibliotecas a incluir */
+#include <stdio.h>
 
 void main()
 {
 	/* Declaración de variables */
+	int len; 
+	char cadena[10] = "cadena\0";
 
-	/* Código usando condicional while */
+	/* Código usando condicional for */
+	for (len = 0; cadena[len] != '\0'; len++)
+	{
+		printf("El carácter es: %c", cadena[len]);
+			
+	} 
+	printf("El número de caracteres es: %d", len);
 }
+
