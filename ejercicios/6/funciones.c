6ea9db6d233655038ad187c93e5dd8a2d4d8b68e
rafavermo <rafavermo@gmail.com>
2015-03-13 15:07:38 +0100

    Resolución Ejercicio 6
    Resolución Ejercicio 6 del archivo funciones.c

--- ejercicios/6/funciones.c
+++ ejercicios/6/funciones.c
@@ -3,16 +3,21 @@
 
 //Pre: Entra una letra
 //Post: Devolver un 1 si la letra es una letra 'a'. Si no, devolver 0.
-tipo_de_dato es_una_A(char letra)
+int es_una_A(char letra)
 {
+	if (letra=='a')
+		return 1;
+	else
+		return 0;
 }
 
-tipo_de_dato main()
+void main()
 {
 	int i;
 	char cadena[40] = "Hola dola pepsi cola\n";
 
-	for (i = 1; i < strlen(cadena); i++) {
+	for (i = 1; i < strlen(cadena); i++) 
+	{
 		if (es_una_A(cadena[i]))
 			printf("En la posición %d tenemos una letra 'a'\n", i);
 	}
