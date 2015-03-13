c090e1eed3271e436776f6e39fea83c93517f804
rafavermo <rafavermo@gmail.com>
2015-03-13 14:59:00 +0100

    Resolución Ejercicio 2
    Switch2.c

--- ejercicios/2/switch2.c
+++ ejercicios/2/switch2.c
@@ -4,10 +4,22 @@
  */
 
 /* Bibliotecas a incluir */
+#include <stdio.h>
 
 void main()
 {
 	/* Declaración de variables */
+    int valor = 1;
 
 	/* Código usando condicional switch */
+    switch (valor)
+	{
+    case 0:
+    case 1:
+		printf("Se ha recibido un 0 o un 1\n");
+		break;
+	default:
+		printf("No es un valor esperado\n");
+		break;
+    }
 }
